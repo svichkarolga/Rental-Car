608606e8666a616368cb004edead4478030629a0
svichkar olga <syns41@gmail.com>
2025-02-26 19:37:17 +0200

    cardItem -done

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
 
-const CarItem = () => {
-  return <div>CarItem</div>;
+const formatAddress = address => {
+  const parts = address.split(', ');
+  return `${parts[1]} | ${parts[2]}`;
+};
+
+const CarItem = ({
+  onCardClick,
+  car: {
+    img,
+    brand,
+    model,
+    year,
+    rentalPrice,
+    address,
+    rentalCompany,
+    type,
+    mileage,
+  },
+}) => {
+  return (
+    <div>
+      <div>
+        <svg width="16" height="15">
+          <use href="/public/icons/LinkedSprite.svg#heart" />
+        </svg>
+        <img src={img} alt={brand} />
+      </div>
+      <h4>
+        {brand} <span>{model}</span>,{year}
+      </h4>
+      <h4>${rentalPrice}</h4>
+      <p>
+        {formatAddress(address)}|{rentalCompany}
+      </p>
+      <p>
+        {type}| {mileage}
+      </p>
+      <div>
+        <button onClick={() => onCardClick(car)}>Read more</button>
+      </div>
+    </div>
+  );
 };
 
 export default CarItem;
--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -3,21 +3,35 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCars } from '../../redux/cars/operations.js';
 import { selectCars } from '../../redux/cars/selectors.js';
+import CarItem from '../CarItem/CarItem.jsx';
 
 const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector(selectCars);
+  const carsData = useSelector(selectCars);
+  const cars = carsData?.cars || [];
+
   useEffect(() => {
     dispatch(fetchCars());
   }, [dispatch]);
+
+  if (!Array.isArray(cars) || cars.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  const handleCardClick = car => {
+    console.log('Clicked car:', car);
+    // Здесь можно реализовать открытие модального окна с детальной информацией
+  };
+
   return (
     <div>
-      {' '}
-      {cars.length > 0 ? (
-        cars.map(car => <div key={car.id}>{car.name}</div>)
-      ) : (
-        <p>Loading...</p>
-      )}
+      <ul>
+        {cars.map(car => (
+          <li key={car.id}>
+            <CarItem car={car} onCardClick={() => handleCardClick(car)} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
