1c219f56dccc35526c5b2c21f5b029bf28002181
svichkar olga <syns41@gmail.com>
2025-03-01 20:26:27 +0200

    calendar done

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import { useId } from 'react';
 import styles from './BookForm.module.css';
+import DatePicker from 'react-datepicker';
+import { enGB } from 'date-fns/locale';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const initialValues = {
   name: '',
@@ -21,35 +24,41 @@ const BookForm = () => {
   };
   return (
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form className={styles.form}>
-        <Field
-          className={styles.field}
-          type="text"
-          name="name"
-          placeholder="Name* "
-        />
-        <Field
-          className={styles.field}
-          type="email"
-          name="email"
-          placeholder="Email* "
-        />
-        <Field
-          className={styles.field}
-          type="date"
-          name="bookingDate"
-          placeholder="Booking date"
-        />
-        <Field
-          as="textarea"
-          className={styles.field1}
-          name="comment"
-          placeholder="Comment"
-        />
-        <button className={styles.btn} type="submit">
-          Send
-        </button>
-      </Form>
+      {({ values, setFieldValue }) => (
+        <Form className={styles.form}>
+          <Field
+            className={styles.field}
+            type="text"
+            name="name"
+            placeholder="Name* "
+          />
+          <Field
+            className={styles.field}
+            type="email"
+            name="email"
+            placeholder="Email* "
+          />
+          <div className={styles.datePickerWrapper}>
+            <DatePicker
+              className={styles.field}
+              selected={values.bookingDate}
+              onChange={date => setFieldValue('bookingDate', date)}
+              dateFormat="dd-MM-yyyy"
+              placeholderText="Booking date"
+              locale={enGB}
+            />
+          </div>
+          <Field
+            as="textarea"
+            className={styles.field1}
+            name="comment"
+            placeholder="Comment"
+          />
+          <button className={styles.btn} type="submit">
+            Send
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
